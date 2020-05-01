2059f58195f7dbaa43057c4f133c4d95b7e28ab4
Max Henkel <max@maxhenkel.de>
2020-05-01 20:42:24 +0000

    Revert "Add config option for other types of shulkerboxes"
    This reverts commit a7d2a1b1607c693614fe62c9b2cd2d8cd4c65aec

--- src/main/java/de/maxhenkel/shulkerbox/Config.java
+++ src/main/java/de/maxhenkel/shulkerbox/Config.java
@@ -1,12 +1,7 @@
 package de.maxhenkel.shulkerbox;
 
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.registries.ForgeRegistryEntry;
 import org.apache.commons.lang3.tuple.Pair;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 public class Config {
 
@@ -28,43 +23,16 @@ public class Config {
 
     public static class ServerConfig {
         public ForgeConfigSpec.BooleanValue onlySneakPlace;
-        public ForgeConfigSpec.ConfigValue<List<? extends String>> shulkerboxWhitelist;
 
         public ServerConfig(ForgeConfigSpec.Builder builder) {
             onlySneakPlace = builder
                     .comment("If the shulkerbox should only be placed when sneaking")
                     .define("only_sneak_place", true);
-            shulkerboxWhitelist = builder
-                    .comment("The items that should be able to be opened")
-                    .comment("Note that this only works with containers that have 27 slots")
-                    .defineList("shulkerbox_whitelist", Arrays.asList(
-                            "minecraft:shulker_box",
-                            "minecraft:white_shulker_box",
-                            "minecraft:orange_shulker_box",
-                            "minecraft:magenta_shulker_box",
-                            "minecraft:light_blue_shulker_box",
-                            "minecraft:yellow_shulker_box",
-                            "minecraft:lime_shulker_box",
-                            "minecraft:pink_shulker_box",
-                            "minecraft:gray_shulker_box",
-                            "minecraft:light_gray_shulker_box",
-                            "minecraft:cyan_shulker_box",
-                            "minecraft:purple_shulker_box",
-                            "minecraft:blue_shulker_box",
-                            "minecraft:brown_shulker_box",
-                            "minecraft:green_shulker_box",
-                            "minecraft:red_shulker_box",
-                            "minecraft:black_shulker_box"
-                    ), Objects::nonNull);
-        }
-
-        public boolean isWhitelistedShulkerbox(ForgeRegistryEntry entry) {
-            return shulkerboxWhitelist.get().contains(entry.getRegistryName().toString());
         }
     }
 
-
     public static class ClientConfig {
+
         public ClientConfig(ForgeConfigSpec.Builder builder) {
 
         }
--- src/main/java/de/maxhenkel/shulkerbox/Utils.java
+++ src/main/java/de/maxhenkel/shulkerbox/Utils.java
@@ -1,11 +1,14 @@
 package de.maxhenkel.shulkerbox;
 
 import de.maxhenkel.shulkerbox.gui.ShulkerboxContainer;
+import net.minecraft.block.Block;
+import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
@@ -17,7 +20,7 @@ public class Utils {
 
     public static ItemStack getShulkerBox(PlayerEntity player, Hand hand) {
         ItemStack stack = player.getHeldItem(hand);
-        if (isOpenableShulkerBox(stack)) {
+        if (isShulkerBox(stack)) {
             return stack;
         }
         return null;
@@ -25,30 +28,24 @@ public class Utils {
 
     public static ItemStack getShulkerBox(PlayerEntity player) {
         ItemStack stack = player.getHeldItem(Hand.MAIN_HAND);
-        if (isOpenableShulkerBox(stack)) {
+        if (isShulkerBox(stack)) {
             return stack;
         }
         stack = player.getHeldItem(Hand.OFF_HAND);
-        if (isOpenableShulkerBox(stack)) {
+        if (isShulkerBox(stack)) {
             return stack;
         }
         return null;
     }
 
-    public static int getLockedSlot(PlayerEntity player) {
-        ItemStack stack = player.getHeldItem(Hand.MAIN_HAND);
-        if (isOpenableShulkerBox(stack)) {
-            return player.inventory.currentItem;
-        }
-        return -1;
-    }
-
-    public static boolean isOpenableShulkerBox(ItemStack stack) {
-        if (stack == null || stack.getCount() != 1) {
+    public static boolean isShulkerBox(ItemStack stack) {
+        if (stack == null) {
             return false;
         }
 
-        if (Config.SERVER.isWhitelistedShulkerbox(stack.getItem())) {
+        Item item = stack.getItem();
+
+        if (Block.getBlockFromItem(item) instanceof ShulkerBoxBlock) {
             return true;
         }
 
