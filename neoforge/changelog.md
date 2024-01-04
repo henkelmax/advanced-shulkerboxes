bd7596604e4af7858da5ddf30f0d2c82359aa0f7
Max Henkel <max@maxhenkel.de>
2024-01-04 12:36:18 +0100

    Update NeoForge

--- neoforge/changelog.md
+++ neoforge/changelog.md
@@ -1,1 +1,1 @@
-- Updated dependencies
+- Updated NeoForge
--- neoforge/src/main/java/de/maxhenkel/shulkerbox/NeoforgeAdvancedShulkerboxesMod.java
+++ neoforge/src/main/java/de/maxhenkel/shulkerbox/NeoforgeAdvancedShulkerboxesMod.java
@@ -1,14 +1,14 @@
 package de.maxhenkel.shulkerbox;
 
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(AdvancedShulkerboxesMod.MODID)
 public class NeoforgeAdvancedShulkerboxesMod extends AdvancedShulkerboxesMod {
 
-    public NeoforgeAdvancedShulkerboxesMod() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+    public NeoforgeAdvancedShulkerboxesMod(IEventBus eventBus) {
+        eventBus.addListener(this::commonSetup);
     }
 
     public void commonSetup(FMLCommonSetupEvent event) {
